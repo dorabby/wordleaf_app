56dbbb7d63a4ddf3ecdaf8d71bea7154980d6b8f
dorabby <dorabbi1010@gmail.com>
2020-02-07 00:52:42 +0900

    book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,34 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
   def new
-
+    @book = Book.new
   end
+
   def create
-
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to root_path
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:name).merge(user_id: current_user.id)
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'words#index'
+  root 'books#index'
   resources :user,only: [:edit,:update]
-  resources :groups, only: [:new, :create]
+  resources :books, only: [:index,:new, :create,:edit,:update]
 end
