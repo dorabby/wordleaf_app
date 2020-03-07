14efb2256b42580eb7439e0c03cedc917a013d1e
dorabby <dorabbi1010@gmail.com>
2020-03-07 23:53:27 +0900

    ドラックアンドドロップ入れ替え２

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -1,17 +1,3 @@
 $(function() {
-  $(".word").sortable();
-  $(".word").disableSelection();
-  $(".word").sortable({
-      update: function(ev, ui) {
-          var updateArray =  $(".word").sortable("toArray").join(",");
-          $.cookie("word", updateArray, {expires: 30});
-      }       
+    $('.word').railsSortable();
   });
-  if($.cookie("word")) {
-      var cookieValue = $.cookie("word").split(",").reverse();
-      $.each(
-          cookieValue,
-          function(index, value) {$('#'+value).prependTo(".word");}
-      );
-  }
-});
