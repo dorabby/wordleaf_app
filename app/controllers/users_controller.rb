cbc9853a23555f8f7c8843d59de466e461aed4a0
dorabby <dorabbi1010@gmail.com>
2020-03-09 12:14:23 +0900

    フラッシュメッセージとREADME修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     if current_user.update(user_params)
       redirect_to root_path
     else
-      render :edit
+      render :edit,aleat:"編集できませんでした"
     end
   end
 
